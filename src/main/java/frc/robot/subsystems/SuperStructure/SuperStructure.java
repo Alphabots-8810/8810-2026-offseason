b63064e86f6d75f63b57481e0186ce136e0a9f9b
927-927 <leozhang12345678910@outlook.com>
2026-06-27 15:39:18 +0800

    Chassis update

---
+++ src/main/java/frc/robot/subsystems/SuperStructure/SuperStructure.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.SuperStructure;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SuperStructure extends SubsystemBase{
+    public static final SuperStructure mInstance = new SuperStructure();
+
+    
+}
