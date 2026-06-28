40aae10add105839b6020e397dff985413437708
HEFZZJM <zhangjiaming147358@gmail.com>
2026-06-28 18:21:17 +0800

    Update ManualConstants.java

--- src/main/java/frc/robot/commands/ManualCommand/ManualConstants.java
+++ src/main/java/frc/robot/commands/ManualCommand/ManualConstants.java
@@ -13,10 +13,15 @@ public final class ManualConstants {
       new LoggedTunableNumber("Manual/IntakeRollerRotps", 40);
   public static final LoggedTunableNumber FeederRotpsTunable =
       new LoggedTunableNumber("Manual/FeederRotps", 40);
-/*
- * 2.118 0 47.5
- * 2.54  5 
- */
+  /*
+   * 2.118 0 47.5
+   * 2.54  5  49.08
+   * 3.05  12 51.08
+   * 3.53 16 53.26
+   * 4.15 20.5 56.
+   * 4.55 23 58
+   * 5.06 26 60
+   */
   public static final double SHOOTER_VELOCITY_TOLERANCE_ROTPS = 1.0;
   public static final double HOOD_ANGLE_TOLERANCE_ROT = 0.1;
 
